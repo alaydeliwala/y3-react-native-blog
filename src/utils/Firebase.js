ab8cc63aa8b3ca5c71f37f99bcd7a7a2279fb197
Alay Deliwala <ahdeliwala@gmail.com>
2023-07-24 08:00:10 +0200

    removed firebase creds

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,15 +1,7 @@
 import firebase from "firebase"
 
 // TODO: Replace this with config from Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDjmrCL6RGw_-Ms2odVALg45gRJr0bQaEs",
-  authDomain: "react-app-playground-fa619.firebaseapp.com",
-  projectId: "react-app-playground-fa619",
-  storageBucket: "react-app-playground-fa619.appspot.com",
-  messagingSenderId: "989038417452",
-  appId: "1:989038417452:web:eda919e90057295d11e389",
-  measurementId: "G-WTB091DFB4"
-};
+const firebaseConfig = {};
 
 function initFirebase(){
   if (!firebase.apps.length){
